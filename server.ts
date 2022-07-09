4f05585ddbe4d8767373e604dc94c1fff9d77792
kurokawa <kurokawa.d@marietta.co.jp>
2022-07-09 18:52:54 +0900

    [Add] サーバー設定のサンプル実装

--- server.ts
+++ server.ts
@@ -1,1 +1,16 @@
-console.log('hello deno !!');
+import {Application, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+import {bold, yellow} from "https://deno.land/std@0.147.0/fmt/colors.ts";
+
+const app = new Application();
+const router = new Router();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener("listen", ({hostname, port}) => {
+  console.log(bold(`Start listening on `)+ yellow(`${hostname?? "localhost"}:${port}`));
+})
+
+const port = parseInt(Deno.env.get("PORT") ?? "8000");
+
+app.listen({port})
