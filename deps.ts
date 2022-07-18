fe3924c1f7bcfe68dce2a6d8cc2eccc68d330af5
kurokawa <kurokawa.d@marietta.co.jp>
2022-07-18 14:40:30 +0900

    [Add] create処理の実装

--- deps.ts
+++ deps.ts
@@ -1,3 +1,8 @@
 export {Application, Router, Status, Context, isHttpError } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
 export type { RouterContext } from "https://deno.land/x/oak@v10.6.0/mod.ts";
+
 export {bold, yellow, cyan, green} from "https://deno.land/std@0.147.0/fmt/colors.ts";
+
+export {v4 as uuid} from "https://deno.land/std@0.148.0/uuid/mod.ts";
+
--- models/todo.ts
+++ models/todo.ts
@@ -1,3 +1,6 @@
+import { toMap } from './utils.ts';
+import { uuid } from '../deps.ts';
+
 interface Todo {
   'id': string,
   'done': boolean,
@@ -12,4 +15,31 @@ export const getAll = async (): Promise<Todo[]> => {
   const data = await Deno.readFile(FILE_PATH);
   const decoder = new TextDecoder();
   return JSON.parse(decoder.decode(data));
+}
+
+type Result<T> = [T, undefined] | [undefined, Error]
+
+export const get = async ({ id }: Pick<Todo, 'id'> ): Promise<Result<Todo>> => {
+const todos = await getAll()
+  const todo = toMap(todos).get(id);
+if (!todo) {
+  return [undefined, new Error('Cannot find item')];
+}
+return [todo, undefined];
+}
+
+export const create = async ({ title }: Pick<Todo, 'title'>): Promise<true> => {
+  const todos = await getAll();
+  const id = uuid.generate();
+  const now = new Date().toISOString();
+
+  await updateAll([...todos, {id, done: false, title, createdAt: now, updatedAt: now}],
+    );
+  return true;
+}
+
+const updateAll = async (todos: Todo[]): Promise<true> => {
+  const encoder = new TextEncoder();
+  Deno.writeFile(FILE_PATH, encoder.encode(JSON.stringify(todos)));
+  return true;
 }
