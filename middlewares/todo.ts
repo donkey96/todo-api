4a56eab85f740d4f68220397c6302d4d1919d143
kurokawa <kurokawa.d@marietta.co.jp>
2022-07-18 16:56:40 +0900

    [Add] statusチェックのutility化

--- middlewares/todo.ts
+++ middlewares/todo.ts
@@ -1,26 +1,19 @@
 import { RouterContext, Status } from '../deps.ts';
 import { todoModel } from '../models/mod.ts';
-import { getParams } from './utils.ts';
+import { getParams, handleError, handleOK } from './utils.ts';
 
 export const getAll = async (ctx: RouterContext<any>) => {
   const todos = await todoModel.getAll();
-  ctx.response.status = Status.OK;
-  ctx.response.body = {
-    data: todos,
-  };
+  handleOK(ctx, todos);
 };
 
 export const get = async (ctx: RouterContext<any>) => {
   const params = await getParams(ctx);
-  const [todo, error] = await todoModel.get(params)
+  const [todo, error] = await todoModel.get(params);
 
   if (error) {
-    ctx.response.status = Status.BadRequest;
-    ctx.response.body = {
-      error: {
-        message: error.message,
-        stack: error.stack,
-      }
-    }
+    return handleError(ctx, error);
   }
-}
+
+  handleOK(ctx, todo);
+};
--- middlewares/utils.ts
+++ middlewares/utils.ts
@@ -1,4 +1,4 @@
-import { RouterContext } from '../deps.ts';
+import { RouterContext, Status } from '../deps.ts';
 
 export const getParams = async (ctx: RouterContext<any>) => {
   const { value } = await ctx.request.body();
@@ -7,4 +7,21 @@ export const getParams = async (ctx: RouterContext<any>) => {
     ...ctx.params,
     ...value,
   }
+}
+
+export const handleOK = (ctx: RouterContext<any>, data: any) => {
+  ctx.response.status = Status.OK;
+  ctx.response.body = {
+    data: data,
+  };
+}
+
+export const handleError = (ctx: RouterContext<any>, error: Error) => {
+    ctx.response.status = Status.BadRequest;
+    ctx.response.body = {
+      error: {
+        message: error.message,
+        stack: error.stack,
+      }
+    }
 }
